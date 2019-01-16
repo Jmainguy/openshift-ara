2c112a7cddbe91b6e6c688ba3810a72bacd54954
Jonathan Mainguy <jon@soh.re>
2019-01-16 12:51:37 -0500

    add ara deployment

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ oc create -f ara-mysql-pvc.yaml
 oc create -f ara-mysql-deployment.yaml
 oc create -f ara-mysql-nodePort.yml
 oc new-build https://github.com/Jmainguy/openshift-ara
+oc create -f ara-deployment.yaml
